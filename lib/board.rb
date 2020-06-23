04bd1d4535edc8d66ae98831e22860031795dcae
Cody C <codyicastro@gmail.com>
2020-06-23 03:59:25 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,3 @@
 # Define the variable board below.
 
 board = [" "," "," "," "," "," "," "," "," "]
-
-cell_1 = "X"
-cell_2= " "
-cell_3 = "O"
-cell_4 = "X"
-cell_5= "O"
-cell_6 = "X"
-cell_7 = "X"
-cell_8= "O"
-cell_9 = "X"
